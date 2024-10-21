026a9af1fda51926b7133a846fdce264b98dddb7
QobilovSardor <note47297@gmail.com>
2024-10-21 13:27:17 +0500

    finished

--- js/script.js
+++ js/script.js
@@ -5,3 +5,10 @@ var swiper = new Swiper('.service-info__swiper', {
   spaceBetween: 20,
   speed: 700,
 });
+
+// categories-swiper
+var swiper = new Swiper('.categories-swiper', {
+  slidesPerView: 2.4,
+  spaceBetween: 12,
+  speed: 700,
+});
