a0305b12418af2dc42e0b194668b5c9d7012f64d
QobilovSardor <note47297@gmail.com>
2024-10-22 20:59:16 +0500

    intro page finished

--- js/script.js
+++ js/script.js
@@ -102,6 +102,10 @@ if (addBasketBtn) {
   });
 }
 
-let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+try {
+  let vh = window.innerHeight * 0.01;
+  // Then we set the value in the --vh custom property to the root of the document
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+} catch (error) {
+
+}
