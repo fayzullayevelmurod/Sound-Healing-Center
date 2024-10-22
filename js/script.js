f139522075a2d47f81ab4624b028c8b4eb99eaa0
QobilovSardor <note47297@gmail.com>
2024-10-22 12:09:35 +0500

    media 320px scces

--- js/script.js
+++ js/script.js
@@ -1,14 +1,42 @@
 "use strict";
 
-var swiper = new Swiper('.service-info__swiper', {
-  slidesPerView: 3.3,
-  spaceBetween: 20,
-  speed: 700,
-});
+try {
+  var swiper = new Swiper('.service-info__swiper', {
+    slidesPerView: 3.3,
+    spaceBetween: 20,
+    speed: 700,
+  });
+} catch (error) {
+}
 
-// categories-swiper
-var swiper = new Swiper('.categories-swiper', {
-  slidesPerView: 2.4,
-  spaceBetween: 12,
-  speed: 700,
-});
+try {
+  // categories-swiper
+  var swiper = new Swiper('.categories-swiper', {
+    slidesPerView: 2.4,
+    spaceBetween: 12,
+    speed: 700,
+  });
+} catch (error) {
+
+}
+
+// counter
+const counters = document.querySelectorAll('.counter');
+counters.forEach(counter => {
+  const increment = counter.querySelector('.increment');
+  const decrement = counter.querySelector('.decrement');
+  const value = counter.querySelector('.count-value');
+  let count = parseInt(value.textContent);
+  console.log(count);
+
+  increment.addEventListener('click', () => {
+    count++;
+    value.textContent = count;
+  });
+  decrement.addEventListener('click', () => {
+    if (count > 0) {
+      count--;
+      value.textContent = count;
+    }
+  })
+})
